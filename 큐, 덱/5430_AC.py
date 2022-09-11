468657293ab1b5db3dd706f4b9a970e43f77aa55
SooYoung <dearsyjang@naver.com>
2022-09-11 22:26:20 +0900

    0911

--- 큐, 덱/5430_AC.py
+++ 큐, 덱/5430_AC.py
@@ -1,46 +1,39 @@
-import sys
-input = sys.stdin.readline
-
 from collections import deque
 
 T = int(input()) # 테스트케이스
 for tc in range(1, T+1):
-    func = sys.stdin.readline().strip()
-    N = int(sys.stdin.readline()) # 수의 개수
-    arr = map(int, sys.stdin.readline().strip()[1:-2].split(','))
+    func = input()
+    N = int(input()) # 수의 개수
+    arr = input()[1:-1].split(',')
     queue = deque(arr)
     reverse = False
+    error = False
 
     if N == 0:
+        queue = deque()
+    for i in func:
+        if i == 'R':
+            if reverse:
+                reverse = False
+            else:
+                reverse = True
+
+        elif i == 'D':
+            if not queue:
+                error = True
+            else:
+                if reverse == True:
+                    queue.pop()
+                else:
+                    queue.popleft()
+
+    if error == True:
         print('error')
-        break
     else:
-        for i in func:
-            if i == 'R':
-                if reverse == False:
-                    reverse = True
-                else:
-                    reverse = False
-
-            elif i == 'D':
-                if not queue:
-                    print('error')
-                    break
-                else:
-                    if reverse == True:
-                        queue.pop()
-                    else:
-                        queue.popleft()
-
-    if not queue:
-        print('error')
-    elif queue:
         if reverse == True:
             queue.reverse()
-            print([*queue])
+            print('[' + ','.join(queue) + ']')
         else:
-            print([*queue])
-
-
+            print('[' + ','.join(queue) + ']')
 
 
